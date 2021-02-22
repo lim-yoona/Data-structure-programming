e0f5a36f81b05b85f91038879aa01c305726c206
lim-yoona <1994238842@qq.com>
2021-02-22 12:56:43 +0800

    Add Newfile

---
+++ commit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "hello world!"
+git add .
+git commit -m 'Add Newfile'
+git config --global  user.email "1994238842@qq.com"
+git config --global  user.name "lim-yoona"
+git push
---
+++ delete.bat
@@ -0,0 +1,4 @@
+del D:\Repositories\PAT-Exercise-Set\*.exe /f /s /q
+::/f 强制删除只读文件
+::/s从所有子目录删除指定文件
+::/q安静模式，删除时不要求确认
